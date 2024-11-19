e2924ca16a97abc2c78a438ad39f20ed096a4e37
Manish Kaushal <manishkaushal163@gmail.com>
2024-11-19 22:39:12 +0530

    loader stuff

--- dusk/src/loaders/gltf_loader.cpp
+++ dusk/src/loaders/gltf_loader.cpp
@@ -1,1 +1,15 @@
 #include "gltf_loader.h"
+
+namespace dusk
+{
+	Unique<Scene> GLTFLoader::readScene(
+		std::string_view fileName, int sceneIndex)
+	{
+
+	}
+
+	Unique<GameObject> GLTFLoader::parseNode(const tinygltf::Node& node)
+	{
+
+	}
+}

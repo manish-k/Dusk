720a9a1a5882b8393c1da48dcd3b343669ee7929
Manish Kaushal <manishkaushal163@gmail.com>
2025-05-14 21:57:56 +0530

    documentation of the GfxBuffer class

--- dusk/src/renderer/gfx_buffer.h
+++ dusk/src/renderer/gfx_buffer.h
@@ -28,6 +28,9 @@ enum GfxBufferMemoryTypeFlags : uint32_t
     DedicatedDeviceMemory = 0x00000008
 };
 
+/**
+ * @brief struct for buffer creation params
+ */
 struct GfxBufferParams
 {
     size_t      sizeInBytes = 0u;
@@ -37,6 +40,9 @@ struct GfxBufferParams
     std::string debugName;
 };
 
+/**
+ * @brief Class to manage graphics buffer object
+ */
 struct GfxBuffer
 {
     VulkanGfxBuffer vkBuffer;
@@ -46,39 +52,138 @@ struct GfxBuffer
     GfxBuffer()                           = default;
     ~GfxBuffer()                          = default;
 
+    /**
+     * @brief initialize the empty buffer
+     * @param usage type of the buffer
+     * @param total size in bytes for allocation
+     * @param type of memory
+     * @param debugName
+     */
     void init(
         uint32_t           usage,
         size_t             sizeInBytes,
         uint32_t           memoryType,
         const std::string& debugName = "");
-    void                   free();
-    bool                   isAllocated() const;
 
-    void                   map();
-    void                   unmap();
-    void                   write(uint32_t offset, void* data, size_t size);
-    void                   writeAndFlush(uint32_t offset, void* data, size_t size);
-    void                   writeAtIndex(uint32_t index, void* data, size_t size);
-    void                   writeAndFlushAtIndex(uint32_t index, void* data, size_t size);
-    void                   flush();
-    void                   flushAtIndex(uint32_t index);
+    /**
+     * @brief release the resources of the buffer
+     */
+    void free();
 
-    void                   copyFrom(const GfxBuffer& srcBuffer, size_t size);
+    /**
+     * @brief Check if buffer was allocated
+     * @return true if allocated else false
+     */
+    bool isAllocated() const;
 
+    /**
+     * @brief map the buffer to host memory
+     */
+    void map();
+
+    /**
+     * @brief unmap host memory
+     */
+    void unmap();
+
+    /**
+     * @brief write data in the mapped buffer at a particular offset
+     * @param offset to start of the writing location
+     * @param source pointer to read from
+     * @param total size to write
+     */
+    void write(uint32_t offset, void* data, size_t size);
+
+    /**
+     * @brief Write data in the mapped buffer and immediately flushes into the device buffer
+     * @param offset to start of the writing location
+     * @param source pointer to read from
+     * @param total size to write
+     */
+    void writeAndFlush(uint32_t offset, void* data, size_t size);
+
+    /**
+     * @brief Write data in the mapped buffer at a particular index(if buffer
+     * is being used as an array)
+     * @param index of the writing location
+     * @param source pointer to read from
+     * @param total size to write
+     */
+    void writeAtIndex(uint32_t index, void* data, size_t size);
+
+    /**
+     * @brief Write data in the mapped buffer at a particular index(if buffer
+     * is being used as an array) and immediately flushes into the device buffer
+     * @param index of the writing location
+     * @param source pointer to read from
+     * @param total size to write
+     */
+    void writeAndFlushAtIndex(uint32_t index, void* data, size_t size);
+
+    /**
+     * @brief Flush all the pending writes in mapped block into the device buffer
+     */
+    void flush();
+
+    /**
+     * @brief Flush all the pending writes in mapped block at a particular index into the device buffer
+     * @param index of the writing location
+     */
+    void flushAtIndex(uint32_t index);
+
+    /**
+     * @brief copy the data from source buffer into the current buffer
+     * @param source buffer
+     * @param total size to write from source buffer
+     */
+    void copyFrom(const GfxBuffer& srcBuffer, size_t size);
+
+    /**
+     * @brief Get descriptor info for writing into the descriptor set for the
+     * current buffer
+     * @param index for which descriptor info is required
+     * @return descriptor buffer info of the indexed instance data
+     */
     VkDescriptorBufferInfo getDescriptorInfoAtIndex(uint32_t index) const;
 
-    static void            createHostWriteBuffer(
-                   uint32_t           usage,
-                   size_t             instanceSize,
-                   uint32_t           instanceCount,
-                   const std::string& debugName,
-                   GfxBuffer*         pOutBuffer);
+    /**
+     * @brief Create buffer to be used by host to write to
+     * @param usage type of the buffer
+     * @param size of a particular instance
+     * @param total instances to allocate
+     * @param debugName
+     * @param pointer to the buffer to allocate
+     */
+    static void createHostWriteBuffer(
+        uint32_t           usage,
+        size_t             instanceSize,
+        uint32_t           instanceCount,
+        const std::string& debugName,
+        GfxBuffer*         pOutBuffer);
+
+    /**
+     * @brief Create buffer to be used by host to read from and write to
+     * @param usage type of the buffer
+     * @param size of a particular instance
+     * @param total instances to allocate
+     * @param debugName
+     * @param pointer to the buffer to allocate
+     */
     static void createHostReadWriteBuffer(
         uint32_t           usage,
         size_t             instanceSize,
         uint32_t           instanceCount,
         const std::string& debugName,
         GfxBuffer*         pOutBuffer);
+
+    /**
+     * @brief Create buffer to be used exclusively by the device only
+     * @param usage type of the buffer
+     * @param size of a particular instance
+     * @param total instances to allocate
+     * @param debugName
+     * @param pointer to the buffer to allocate
+     */
     static void createDeviceLocalBuffer(
         uint32_t           usage,
         size_t             instanceSize,
@@ -87,5 +192,11 @@ struct GfxBuffer
         GfxBuffer*         pOutBuffer);
 };
 
+/**
+ * @brief Calculate alignment for each instances as per vulkan limits
+ * @param usage type of the buffer
+ * @param size of a particular instance
+ * @return alignmentsize of the instance
+ */
 uint32_t getBufferOffsetAlignment(uint32_t usage, size_t instanceSize);
 } // namespace dusk

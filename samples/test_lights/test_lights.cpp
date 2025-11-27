17f6f015b6155fd5500b4dda2c455dc27dfb79b5
Manish Kaushal <manishkaushal163@gmail.com>
2025-11-27 21:33:41 +0530

    test scene

--- samples/test_lights/test_lights.cpp
+++ samples/test_lights/test_lights.cpp
@@ -4,6 +4,7 @@
 #include "scene/components/transform.h"
 #include "scene/components/camera.h"
 #include "scene/components/lights.h"
+#include "scene/camera_controller.h"
 
 TestLights::TestLights()
 {
@@ -27,48 +28,45 @@ bool TestLights::start()
     m_testScene           = Scene::createSceneFromGLTF(scenePath);
 
     // adding ambient light
-    auto ambientLight = dusk::createUnique<GameObject>();
+    /*auto ambientLight = dusk::createUnique<GameObject>();
     ambientLight->setName("ambient_light");
     auto& aLight = ambientLight->addComponent<AmbientLightComponent>();
     aLight.color = glm::vec4(1.f, 1.f, 1.f, 0.1);
-    m_testScene->addGameObject(std::move(ambientLight), m_testScene->getRootId());
+    m_testScene->addGameObject(std::move(ambientLight), m_testScene->getRootId());*/
 
     // adding directional light
     auto directionalLight = dusk::createUnique<GameObject>();
     directionalLight->setName("directional_light_0");
     auto& dLight     = directionalLight->addComponent<DirectionalLightComponent>();
-    dLight.direction = glm::vec3(4.f, -2.f, -6.f);
-    dLight.color     = glm::vec4(1.f, 1.f, 1.f, 0.2);
+    dLight.direction = glm::vec3(-1.f, -1.f, -1.f);
+    dLight.color     = glm::vec4(1.f, 1.f, 1.f, 0.9);
     m_testScene->addGameObject(std::move(directionalLight), m_testScene->getRootId());
 
     // adding point light
-    auto pointLight = dusk::createUnique<GameObject>();
+    /*auto pointLight = dusk::createUnique<GameObject>();
     pointLight->setName("point_light_0");
     auto& pointTransform       = pointLight->getComponent<TransformComponent>();
     pointTransform.translation = glm::vec3(0.f, 3.f, 0.f);
     auto& pLight               = pointLight->addComponent<PointLightComponent>();
     pLight.color               = glm::vec4(1.f, 1.f, 1.f, 0.6);
-    m_testScene->addGameObject(std::move(pointLight), m_testScene->getRootId());
+    m_testScene->addGameObject(std::move(pointLight), m_testScene->getRootId());*/
 
     // adding spot light
-    auto spotLight = dusk::createUnique<GameObject>();
-    spotLight->setName("spot_light_0");
-    auto& spotTransform       = spotLight->getComponent<TransformComponent>();
-    spotTransform.translation = glm::vec3(3.f, 1.5f, 0.f);
-    auto& sLight              = spotLight->addComponent<SpotLightComponent>();
-    sLight.color              = glm::vec4(1.f, 1.f, 1.f, 0.8);
-    sLight.direction          = glm::vec3(0.f, -2.f, 0.f);
-    sLight.innerCutOff        = 0.86f; // 30 degrees
-    sLight.outerCutOff        = 0.8f;  // ~35 degrees
-    m_testScene->addGameObject(std::move(spotLight), m_testScene->getRootId());
+    //auto spotLight = dusk::createUnique<GameObject>();
+    //spotLight->setName("spot_light_0");
+    //auto& spotTransform       = spotLight->getComponent<TransformComponent>();
+    //spotTransform.translation = glm::vec3(3.f, 1.5f, 0.f);
+    //auto& sLight              = spotLight->addComponent<SpotLightComponent>();
+    //sLight.color              = glm::vec4(1.f, 1.f, 1.f, 0.8);
+    //sLight.direction          = glm::vec3(0.f, -2.f, 0.f);
+    //sLight.innerCutOff        = 0.86f; // 30 degrees
+    //sLight.outerCutOff        = 0.8f;  // ~35 degrees
+    //m_testScene->addGameObject(std::move(spotLight), m_testScene->getRootId());
 
     Engine::get().loadScene(m_testScene.get());
 
-    auto& cameraTransform       = m_testScene->getMainCameraTransform();
-    auto& camera                = m_testScene->getMainCamera();
-
-    cameraTransform.translation = { 3.f, 2.3f, 10.f };
-    camera.setViewTarget(cameraTransform.translation, glm::vec3 { 0.f }, { 0.f, 1.f, 0.f });
+    auto& cameraController = m_testScene.get()->getMainCameraController();
+    cameraController.setViewTarget({ 10.f, 10.f, 10.f }, { 0.f, 0.f, 0.f });
 
     return true;
 }
